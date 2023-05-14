841aaec6eb63473a0334f73d3ac1c94efff73766
Juan <juan@gmail.com>
2023-05-15 02:48:28 +0700

    add file

---
+++ public/home.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBSITE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<header>
+<nav class="navbar navbar-expand-lg navbar navbar-light fixed-top bg-light shadow-sm border-bottom">
+  <div class="container-lg">
+    <a class="navbar-brand text-secondary fw-semibold fs-3" href="#">TRANQUILLERO</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5 text-center">
+        <li class="nav-item">
+          <a class="nav-link active text-secondary fw-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-label;edby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Web Design</a></li>
+            <li><a class="dropdown-item" href="#">Programmer</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Multimedia Developer</a></li>
+          </ul>
+        </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Our Team</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/n.jpg" class="image-flued w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/e.jpg" class="image-fluid w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/a.jpg" class="image-fluid w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</header>
+
+<section class="about my-5">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fst-italic text-center" >About<span class="text-primary fst-italic"> Me</span></h1>
+            <hr / class="w-25 m-auto">
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6 col-12">
+                <h1 class="fst-italic fs-2">What do you <span class="text-primary fst-italic"> want to know?</span></h1>
+                <p class="p-2">Information technology is responsible for such a large portion of our workforce, business operations and personal access to information that it comprises much of our daily activities.</p>
+                <button type="button" class="btn btn-light">More about me</button>
+
+
+                <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Motto
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>"Do the best you can until you know better. Then, when you know better, do better." -Jenamel</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        My Dreams
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>To Earn Passive Income</p>
+        <p>Becoming What you want</p>
+        <p>To Travel around the World</p>
+        <p>To Live in another Country</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Future Me
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Everything that has happened has lead you to this moment. Don’t change a thing. Everything worked out exactly as it should be. All the puzzle pieces fit, all the paths make sense. All the people were meant to be. All the successes and failures and heartwarming and heartbreaking moments were all worth it. No regrets. You were and are and have been and always will be yourself, and that’s what matters above all.</p>
+        <p>I’m sure you’ve got a lot going on right now, and that’s good. Just don’t lose track of what’s important, what’s worth making time for. Spend time with people you love. </p>
+    </div>
+    </div>
+  </div>
+</div>
+            </div>
+           
+            <div class="col-sm-12 col-md-6 col-lg-6 col-12 m-auto text-end">
+                <img src="image/love.jpg" class="img-fluid img-thumbnail">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services py-5 bg-light">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fst-italic">Our <span class="text-primary fst-italic"> Services</span></h1>
+            <hr / class="w-25 m-auto">
+        </div>
+        <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center text-primary">Web Design</h5>
+                <p>"If you think math is hard, try web design"</p>
+                <p class="card-text">In our web designer we have <span class="fw-semibold">MEGA LANALEE ARBOLADO</span> and <span class="fw-semibold">MYRA ACOSTA</span> one of the best when it comes to designs i can assure you that your website will be attractive and wonderful</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center text-primary">Programmer</h5>
+                <p>"Programming isn't about what you know; it's about what you can figure out.”</p>
+                <p class="card-text">programming is not easy if you don't want to learn, but to <span class="fw-semibold">JERHEL RAMOS</span>, <span class="fw-semibold">ERICKA FAYE TOLENTINO</span>, and <span class="fw-semibold"> MYRA ACOSTA</span>, nothing is impossible</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card">
+            <div class="card-body ">
+                <h5 class="card-title text-center text-primary">Multimedia Designer</h5>
+                <p>"Design can be art. Design can be aesthetics. Design in so simple, that's why it is so complicated".</p>
+                <p class="card-text">"Styles come and go. Good design is a language, not a style".</p>
+            </div>
+        </div>
+        </div>
+        </div>
+    </div>
+</section>
+
+<section class="contact">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fst-italic">Feel Free <span class="text-primary fst-italic"> to Contact</span></h1>
+            <hr / class="w-25 m-auto">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6 col-12">
+        <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+</div>
+
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
